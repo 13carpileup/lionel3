b755bef99faad79f58d3495916062078d8279977
Alex Climie <55697723+13carpileup@users.noreply.github.com>
2022-09-21 21:18:25 +0800

    morbification

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,10 +1,12 @@
 import Counter from "../islands/Counter.tsx";
 
+
+
 export default function Home() {
   return (
     <div class="p-4 mx-auto max-w-screen-md">
       <img
-        src="/logo.svg"
+        src="/logo.jpg"
         class="w-32 h-32"
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
