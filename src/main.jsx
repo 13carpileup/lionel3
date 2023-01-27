dbd94ff589523b9b8604de72c862d956622d971d
Alex Climie <55697723+13carpileup@users.noreply.github.com>
2023-01-27 17:59:36 +0800

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter><App /></BrowserRouter>
+    <HashRouter><App /></HashRouter>
   </React.StrictMode>,
 )
