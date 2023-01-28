12e9966f42ae3d1a37e473bf7552b5fffcfb7a1b
Alex Climie <55697723+13carpileup@users.noreply.github.com>
2023-01-28 17:50:41 +0800

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { Routes, Route, Link } from 'react-router-dom'
 import {Home} from "./pages/home"
 import { Countdown } from './pages/countdown'
 import { UserSchedule } from './pages/userSchedule'
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <>
+    <Analytics />
     <nav>
       <ul>
       <li><Link to="/">Lionel 3</Link></li>
