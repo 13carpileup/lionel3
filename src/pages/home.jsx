1f446b812c4272aa8d5665b4f1860b6c02bb69a9
Alex Climie <55697723+13carpileup@users.noreply.github.com>
2023-02-14 14:01:16 +0800

    New change

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,8 @@ export function Home() {
         <>
         <h1>Lionel 3</h1>
         <p class="subheading">It's like lionel 2, but better.</p>
+
+        <a>I love Lionel3!</a>
         </>
     )
 }
