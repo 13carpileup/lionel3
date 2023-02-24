9984a04597648067703d0f56f36be868ba83cd40
Alex Climie <55697723+13carpileup@users.noreply.github.com>
2023-02-24 09:03:47 +0800

    oral countdown
    why have 1800 files changed

--- src/pages/countdown.jsx
+++ src/pages/countdown.jsx
@@ -10,6 +10,7 @@ export function Countdown() {
     const [seconds, setSeconds] = useState([]);
 
     const deadline = [
+    ["March, 27, 2023, 8:15","Spanish Orals"],
     ["April, 14, 2023, 3:20","Study Leave"],
     ["April, 26, 2023, 8:30","Chinese 2nd Lang"],
     ["April, 28, 2023, 8:30","Chinese 2nd Lang"],
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,71 @@
+import { Formik } from 'formik';
+
+ 
+
 export function Home() {
+    
     return (
         <>
-        <h1>Lionel 3</h1>
-        <p class="subheading">It's like lionel 2, but better.</p>
-
-        <a>I love Lionel3!</a>
+        <div>
+            <h1>Lionel 3</h1>
+            <p class="subheading">It's like lionel 2, but better.</p>
+            <Formik
+            initialValues={{ email: '', password: '' }}
+            validate={values => {
+                const errors = {};
+                if (!values.email) {
+                errors.email = 'Required';
+                } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                ) {
+                errors.email = 'Invalid email address';
+                }
+                return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+                }, 400);
+            }}
+            >
+            {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+                /* and other goodies */
+            }) => (
+                <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                />
+                {errors.email && touched.email && errors.email}
+                <input
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                />
+                {errors.password && touched.password && errors.password}
+                <button type="submit" disabled={isSubmitting}>
+                    Submit
+                </button>
+                </form>
+            )}
+            </Formik>
+        </div>
         </>
     )
 }
+
+
+
