a09a6080b0403dddecd717ec97c4171ad028470c
13carpileup <climie.alex@gmail.com>
2024-06-23 21:58:54 +0800

    timetable api endpoint
    idk if it works lol

--- api/src/main.rs
+++ api/src/main.rs
@@ -6,7 +6,12 @@ use axum::{
 };
 
 use serde::{Deserialize, Serialize};
+use serde_json::to_string;
 use std::fs;
+use std::fs::File;
+use std::io::Read;
+use std::io::BufReader;
+use std::io::prelude::*;
 use tower_http::{cors::CorsLayer, trace::TraceLayer};
 
 // baby structs
@@ -23,17 +28,44 @@ struct Student {
 }
 
 
+
+// -----------------
 // STUDENT GETTERS
+// -----------------
+
+//student list
 fn all_students() -> Vec<Student> {
     let students_json = fs::read_to_string("students.json").unwrap();
     serde_json::from_str(&students_json).unwrap()
 }
 
+//timetable
+fn get_timetable(student_id: u64) -> String {
+    let mut base_path: String = "/timetables/ ".to_owned();
+    let id: String = student_id.to_string().to_owned();
+    let file_ext: String = ".ics".to_owned();
+
+    base_path.push_str(&id);
+    base_path.push_str(&file_ext);
+
+
+    let mut file = File::open(base_path).expect("Unable to open");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents);
+    return contents;
+}
+
+// /students/:id/
 async fn student(Path(student_id): Path<u64>) -> Json<Student> {
     let students = all_students();
     Json(students.into_iter().find(|student| student.id == student_id).unwrap())
 }
 
+// /students/timetable/:id
+async fn timetable(Path(student_id): Path<u64>) -> String {
+    let timetable = get_timetable(student_id);
+    return timetable
+}
 
 
 // main
@@ -45,6 +77,7 @@ async fn main() {
     .route("/foo", get(get_foo).post(post_foo))
     .route("/foo/bar", get(foo_bar))
     .route("/students/:student_id", get(student))
+    .route("/students/timetable/:student_id", get())
     .layer(TraceLayer::new_for_http())
     .layer(CorsLayer::permissive());
 
