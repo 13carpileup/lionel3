113aea98b643051d2b12d55fa99039178e5b8c43
13carpileup <climie.alex@gmail.com>
2024-06-25 10:11:12 +0800

    working timetable api
    might need to add workins with calendar apis to correct week alignments but that is future me problem

--- api/target/debug/api.d
+++ api/target/debug/api.d
@@ -1,1 +1,1 @@
-/home/alex/Documents/Code/lionel3/api/target/debug/api: /home/alex/Documents/Code/lionel3/api/src/main.rs
+/home/alex/Documents/Code/lionel3/api/target/debug/api: /home/alex/Documents/Code/lionel3/api/src/main.rs /home/alex/Documents/Code/lionel3/api/src/structs.rs /home/alex/Documents/Code/lionel3/api/src/timetable.rs
