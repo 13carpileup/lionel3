8a7761c96c77a571b9921d4a4f4e64df403958d1
13carpileup <climie.alex@gmail.com>
2024-07-02 13:22:21 +0800

    hw framework

--- api/src/homework.rs
+++ api/src/homework.rs
@@ -1,13 +1,14 @@
 use std::str;
 use std::thread;
-use scraper::{Html, Selector};
+use substring::Substring; //don't wanna implement that
+
 
 #[tokio::main]
 async fn fetch_homework(student_id: u64) -> Result<String, Box<dyn std::error::Error>>  {
     let students = super::all_students();
     let student = students.into_iter().find(|student| student.id == student_id).unwrap();
 
-    let url = format!("https://lionel2.kgv.edu.hk/local/mis/mobile/homeworkcal.php/9668/{weird_string}.ics", weird_string = student.lionel_string);
+    let url = format!("https://lionel2.kgv.edu.hk/local/mis/mobile/homeworkcal.php/{student_id}/{weird_string}.ics", weird_string = student.lionel_string);
 
     let response = reqwest::get(url).await?;    
     let bytes_response: axum::body::Bytes = response.bytes().await?;
@@ -18,15 +19,40 @@ async fn fetch_homework(student_id: u64) -> Result<String, Box<dyn std::error::E
     Ok(string_response.to_string())
 }
 
-pub fn get_homework(student_id: u64) -> Vec<super::structs::Homework> {
-    let classes = super::student_helpers::get_classes(student_id);
+fn parse_homework(ics_file: String) -> Vec<super::structs::Homework> {
+    let split_string = ics_file.split("\n");
 
+    let mut current = super::structs::Homework {
+        class: "Error".to_string(),
+        due_date: "Error".to_string(),
+        text: "Error".to_string(),
+        time: "Error".to_string()
+ 
+    }
+
+    let output: Vec<super::structs::Homework> = Vec::new();
+
+    for line in split_string {
+        if line.starts_with("DESCRIPTION") {
+            current.text = line.substring(12, line.chars().count()).to_string();
+        }
+
+        if line.starts_with("CLASS") {
+            current.class = line.substring(6, line.chars().count()).to_string();
+        }
+    }
+
+    output
+}
+
+pub fn get_homework(student_id: u64) -> Vec<super::structs::Homework> {
+    // let classes = super::student_helpers::get_classes(student_id);
     
     thread::spawn(move || -> Vec<super::structs::Homework> {
         let hw = fetch_homework(student_id);
 
         match hw {
-            Ok(_v) => return vec!(),
+            Ok(v) => return parse_homework(v),
             Err(_e) => return vec!(),
         }
     }).join().expect("Thread panicked");
